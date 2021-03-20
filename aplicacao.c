abc260dc480d72010191d21af45c236f4abe3958
marco-developer <marcoantonio.marques@gmail.com>
2021-03-20 03:11:40 -0300

    Switch tipo dado

--- aplicacao.c
+++ aplicacao.c
@@ -5,37 +5,81 @@
 int main(int argc, char *argv[]) {
     
     pilha *P;
-    int *arg;
-    int tamdado;
-    void *removido;
+    // int *arg;
+    // int tamdado;
+    int menu;
     int nroElementos;
     void *insere;
-
-    printf ("Informe o tamanho do dado: \n");
-    scanf ("%i", &tamdado);
-
-    printf ("Informe o numero de elementos: \n");
-    scanf ("%i", &nroElementos);
+    void *remove;
     
 
-    printf ("Preparando para criar pilha...\n");
-    P = createStack(nroElementos, sizeof(tamdado));
-    printf ("Pilha criada.\n");
+    // printf ("Informe o tamanho do dado: \n");
+    // scanf ("%d", &tamdado);
 
+    printf ("Informe o numero de elementos: \n");
+    scanf ("%d", &nroElementos);
+    
 
-    printf ("Iniciando empilhamento...\n\n");
-    for(int i=0;i<nroElementos;i++){
-        printf ("Digite o valor a inserir: \n");
-        scanf ("%d", &insere);
-        push(P, &insere);
+    // printf ("Preparando para criar pilha...\n");
+    // P = createStack(nroElementos, sizeof(tamdado));
+    // printf ("Pilha criada.\n");
+    // printf ("Iniciando empilhamento...\n\n");
+
+    printf ("Escolha o tipo de dado: (1)-int, (2)-float \n");
+    scanf ("%d", &menu);
+    
+    switch (menu) {
+        case 1:
+        printf ("Preparando para criar pilha...\n");
+        P = createStack(nroElementos, sizeof(int));
+        printf ("Pilha criada.\n");
+
+        printf ("Iniciando empilhamento...\n\n");
+        for(int i=0;i<nroElementos;i++){
+            printf ("Digite o valor a inserir: \n");
+            scanf ("%d", &insere);
+            push(P, &insere);
+        }
+
+        printf ("Iniciando desempilhamento...\n\n");
+        for(int i=0;i<nroElementos;i++){    
+            pop(P, &remove);
+            printf ("Item removido: %d\n", remove);
+        }
+        break;
+        
+        case 2:
+        printf ("Preparando para criar pilha...\n");
+        P = createStack(nroElementos, sizeof(float));
+        printf ("Pilha criada.\n");
+
+        printf ("Iniciando empilhamento...\n\n");
+        for(int i=0;i<nroElementos;i++){
+            printf ("Digite o valor a inserir: \n");
+            scanf ("%f", &insere);
+            push(P, &insere);
+        }
+        
+        printf ("Iniciando desempilhamento...\n\n");
+        for(int i=0;i<nroElementos;i++){    
+            pop(P, &remove);
+            printf ("Item removido: %f\n", remove);
+        }
+        break;
     }
 
-    printf ("Iniciando desempilhamento...\n\n");
+    // for(int i=0;i<nroElementos;i++){
+    //     printf ("Digite o valor a inserir: \n");
+    //     scanf ("%d", &insere);
+    //     push(P, &insere);
+    // }
 
-    for(int i=0;i<nroElementos;i++){    
-        pop(P, &removido);
-        printf ("Item removido: %d\n", removido);
-    }
+    // printf ("Iniciando desempilhamento...\n\n");
+
+    // for(int i=0;i<nroElementos;i++){    
+    //     pop(P, &removido);
+    //     printf ("Item removido: %d\n", removido);
+    // }
 
      // freeStack(P);
 
